f3db02c81f5fe77554df9ae1de949a37d4f5f3d8
Memil <50459192+yss1902@users.noreply.github.com>
2026-01-12 09:28:25 +0900

    first

---
+++ src/main/java/com/Link_Lotion_beta/LinkLotionBetaApplication.java
@@ -0,0 +1,13 @@
+package com.Link_Lotion_beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkLotionBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkLotionBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Link_Lotion_beta/LinkLotionBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Link_Lotion_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkLotionBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
